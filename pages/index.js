c692268762fb1c934961922754d9f51794bbc3a3
r4lfl4b <dev@rgalan.es>
2019-10-29 12:29:17 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Layout from '../components/MyLayout';
+
+const Index = () => (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+);
+
+export default Index;
